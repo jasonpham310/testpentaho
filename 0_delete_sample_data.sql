7bf68f0491f1d756ed941309ce2e523d7f03c02c
jasonpham310 <98005929+jasonpham310@users.noreply.github.com>
2022-01-19 13:05:35 +0700

    Add files via upload

---
+++ 0_delete_sample_data.sql
@@ -0,0 +1,17 @@
+delete from contact_comment
+
+delete from contact
+
+delete from company
+
+delete from vertical
+
+delete from user_account_vertical
+
+delete from sub_functional_expertise
+
+delete from functional_expertise
+
+delete from activity
+
+select * from contact_comment
